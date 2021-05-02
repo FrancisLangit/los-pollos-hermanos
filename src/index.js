13a60b1e88abfefd4650cb5cc29fa7b9b26f1991
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-02 16:07:55 +0800

    Add banner images to repository
    Create /src/data folder to hold non-code files.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-let content = document.getElementById('content');
-content.textContent = 'Content';
+const index = (() => {
+
+    const setUp = () => {
+        let bannerBackground = document.createElement('img');
+        bannerBackground.src = '/src/data/bannerBackground.jpg';
+        bannerBackground.classList.add('bannerImage');
+
+        let bannerLogo = document.createElement('img');
+        bannerLogo.src = '/src/data/bannerLogo.png';
+        bannerLogo.classList.add('bannerImage');
+
+        let bannerDiv = document.getElementById('banner');
+        bannerDiv.appendChild(bannerBackground);
+        bannerDiv.appendChild(bannerLogo);
+    }
+
+    return { setUp }
+})();
+
+index.setUp();
