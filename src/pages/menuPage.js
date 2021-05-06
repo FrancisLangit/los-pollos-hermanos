45aec1462a353f4ee295bb13c20300bbd5ba57e1
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-06 15:06:17 +0800

    Refactor files in dist/styles directory

--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -3,21 +3,35 @@ import { createImage, createText } from '../helpers.js';
 const menuPage = (() => {
     /**Page featuring food menu of Los Pollos Hermanos. */
     
-    const _createMenuItem = (imageFileName, price, description) => {
+    const _createMenuItem = (imageFileName, name, price, description) => {
         /**Returns a node containing the image, price, and description of an
          * item in the main menu.
          * 
          * Args:
          *  imageFileName (string): Filename of the menu item's image in 
          *      /src/data/images directory.
+         *  name (string): Text serving a name of the menu item.
          *  price (string): Text serving as price to be displayed.
          *  description (string): Text serving as description of menu item.*/
         let menuItemContainer = document.createElement('div');
         menuItemContainer.classList.add('menuItem');
 
-        menuItemContainer.appendChild(createImage(imageFileName));
-        menuItemContainer.appendChild(createText('p', price));
-        menuItemContainer.appendChild(createText('p', description));
+        let menuItemImage = createImage(imageFileName)
+        menuItemImage.classList.add('menuItemImage');
+
+        let menuItemName = createText('p', name);
+        menuItemName.classList.add('menuItemName');
+
+        let menuItemPrice = createText('p', price);
+        menuItemPrice.classList.add('menuItemPrice');
+
+        let menuItemDescription = createText('p', description);
+        menuItemDescription.classList.add('menuItemDescription');
+
+        menuItemContainer.appendChild(menuItemImage);
+        menuItemContainer.appendChild(menuItemName);
+        menuItemContainer.appendChild(menuItemPrice);
+        menuItemContainer.appendChild(menuItemDescription);
 
         return menuItemContainer;
     }
@@ -27,23 +41,27 @@ const menuPage = (() => {
         let menuContainer = document.createElement('div');
         menuContainer.appendChild(_createMenuItem(
             'pollosTenders.jpg',
+            'Pollos Tenders',
             '$12.95',
             `Crispy tenders fried to perfection in our secret batter recipe,
             this large portion is sure to satisfy anyone's appetite.`,
         ));
         menuContainer.appendChild(_createMenuItem(
             'abqHotChickenSandwich.jpg',
+            'ABQ Hot Chicken Sandwich',
             '$12.95',
             `A delicious Fried Chicken Sandwich smothered in spicy seasoning
             and topped off in a Kings Hawaiian Bun.`,
         ));
         menuContainer.appendChild(_createMenuItem(
             'herrSchulersFryFrenzy.jpg',
+            'Herr Schulers Fry Frenzy',
             '$12.00',
             `Yummy seasoned curly fries with a crunch in every bite.`,
         ));
         menuContainer.appendChild(_createMenuItem(
             'slawGoodman.jpg',
+            'Slaw Goodman',
             '$3.00',
             `Our tangy and flavorful slaw, good on it's own but even better
             when paired with the ABQ Chicken Sandwich.`,
