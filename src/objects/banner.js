753da635147b921722afcdaf23d1df9f0368e2d6
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-09 09:20:12 +0800

    Refactor creating banner image in banner.js
    Created _createBannerLogo to replace logic of doing so.

--- src/objects/banner.js
+++ src/objects/banner.js
@@ -1,5 +1,3 @@
-import { createImage } from '../helpers.js';
-
 import bannerLogoImage from '../data/images/bannerLogo.png';
 
 const banner = (() => {
@@ -13,15 +11,19 @@ const banner = (() => {
         return bannerContainer;
     }
 
+    const _createBannerLogo = () => {
+        /**Returns image node with src as bannerLogoImage imported above.*/
+        let bannerLogoNode = document.createElement('img');
+        bannerLogoNode.src = bannerLogoImage;
+        bannerLogoNode.id = 'bannerLogo';
+        return bannerLogoNode;
+    }
+
     const create = () => {
         /**Creates the banner node and inserts such into the webpage before 
          * the #contents div.*/
         let bannerContainer = _createBannerContainer();
-        
-        let bannerLogo = createImage('bannerLogo', 'bannerLogo');
-
-        bannerContainer.appendChild(bannerLogo);
-
+        bannerContainer.appendChild(_createBannerLogo());
         document.body.insertBefore(
             bannerContainer, document.getElementById('content'));
     }
