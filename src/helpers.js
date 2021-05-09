e73c4527822a42e1fbfeaf42355ec5b3a42377f5
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-09 09:29:24 +0800

    Delete createImage function from helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,31 +1,3 @@
-import bannerLogoImage from './data/images/bannerLogo.png';
-import restaurantFacade1Image from './data/images/restaurantFacade1.jpeg';
-import restaurantFacade2Image from './data/images/restaurantFacade2.jpg';
-import restaurantFoodImage from './data/images/restaurantFood.jpg';
-import restaurantMascotImage from './data/images/restaurantMascot.jpg';
-
-const createImage = (fileKey, id) => {
-    /**Returns an image node whose src is "filename" and whose id is "id".
-     * 
-     * Args:
-     *  fileKey (string): Key of the image in imageFiles object below.
-     *  id (string) [optional]: ID of the image node to be returned.*/
-
-    const imageFiles = {
-        'bannerLogo': bannerLogoImage,
-        'restaurantFacade1': restaurantFacade1Image,
-        'restaurantFacade2': restaurantFacade2Image,
-        'restaurantFood': restaurantFoodImage,
-        'restaurantMascot': restaurantMascotImage,
-    }
-    let image = document.createElement('img');
-    image.src = imageFiles[fileKey];
-    if (id) {
-        image.id = id;
-    }
-    return image;
-}
-
 const createText = (elementType, text) => {
     /**Returns a text node whose element type is "elementType" and whose text
      * content is equal to "text".*/
@@ -34,4 +6,4 @@ const createText = (elementType, text) => {
     return textNode; 
 }
 
-export { createImage, createText }
+export { createText }
